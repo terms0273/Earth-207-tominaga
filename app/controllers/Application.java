6d00b0fa93af2b3c693185fc626da9b2b106f930
Ryota Tominaga <r-tominaga@ttc-net.co.jp>
2017-05-18 12:51:46 +0900

    ログインページにフォーム出現

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,12 +2,26 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import play.data.*;
+import static play.data.Form.*;
 import views.html.*;
 
 public class Application extends Controller {
+
+  //Form用内部クラス
+  public static class LoginForm{
+    public String loginForm;
+  }
+
   public static Result index(){
-    return ok(loginpage.render());
+    return ok(loginpage.render("Login", new Form(LoginForm.class)));
+  }
+
+  public static Result send(){
+    Form<LoginForm> f = form(LoginForm.class).bindFromRequest();
+
+    LoginForm data = f.get();
+    return ok(loginpage.render("1234", f));
   }
 
 }
